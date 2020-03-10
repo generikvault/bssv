2f8f2146ef7db2bbe7485e680ef4168d545ce5e4
Jan Erik Keller <jan-erik.keller@helmsauer-gruppe.de>
2020-03-10 15:02:31 +0100

    added block cation to statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as vscode from 'vscode';
 
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.languages.registerDocumentFormattingEditProvider('bssv', new Formatter())
+		vscode.languages.registerDocumentFormattingEditProvider('bssv', new Formatter()),
+		new StatusBarCollInfo(),
 	);
 }
 
@@ -20,7 +20,7 @@ class Formatter {
 		var delta: vscode.TextEdit[] = []
 		for (var i = 0; i < lineCount; i++) {
 			var line = lineAt(i).text
-			if (line.substring(0,1) === "[") {
+			if (line.substring(0, 1) === "[") {
 				cellStarts = undefined
 				continue
 			}
@@ -58,5 +58,67 @@ class Formatter {
 		}
 		return delta
 	}
+}
 
+class StatusBarCollInfo implements vscode.Disposable {
+
+	private disposables: vscode.Disposable[] = []
+	private statusBarItem: vscode.StatusBarItem
+
+	constructor() {
+		
+		this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
+		this.disposables.push(
+			vscode.window.onDidChangeTextEditorSelection(e => this.update(e.textEditor)),
+		)
+	}
+
+	private update(editor: vscode.TextEditor) {
+		this.statusBarItem.text = this.colname(editor)
+		this.statusBarItem.show()
+	}
+
+	private colname(editor: vscode.TextEditor): string {
+		var { document } = editor
+		var { active } = editor.selection
+		var l = active.line
+		var line = document
+			.lineAt(l)
+			.text
+		var commentStart = line.indexOf("//")
+		var contentLength = commentStart < 0 ? line.length : commentStart
+		if (active.character > contentLength)
+			return ""
+		var content = line.substring(0, contentLength)
+		if (content.indexOf(";") < 0)
+			return ""
+		var cellId = content
+			.substring(0, active.character)
+			.split(";")
+			.length - 1
+
+		var colname = ""
+
+		while (l > 0) {
+			l --;
+			var line = document
+				.lineAt(l)
+				.text
+			if (line.startsWith("["))
+				return line.substring(1, line.indexOf("]")) + colname
+			var isAnnotation = line.endsWith("@Header")
+			if(!isAnnotation)
+				continue
+			var commentStart = line.indexOf("//", line.indexOf("//") + 2)
+			var contentLength = commentStart < 0 ? line.length : commentStart
+			var cells = line.substring(0, contentLength).split(";")
+			colname = " - " + (cells[cellId] ?? "").replace("//", "").trim()
+		}
+		return ""
+	}
+
+	dispose() {
+		this.statusBarItem.dispose()
+		this.disposables.forEach(d => d.dispose())
+	}
 }
